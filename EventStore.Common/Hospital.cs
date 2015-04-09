f1641816d9270359c0a0d2534fc2c2cc18e6460f
Justin <justin.joyce@verizon.net>
2015-04-09 12:52:40 -0400

    add new files

---
+++ EventStore.Common/Hospital.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventStore.Common
+{
+    public class Hospital
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string CountyName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Type { get; set; }
+        public string Ownership { get; set; }
+        public bool EmergencyServices { get; set; }
+    }
+}
---
+++ EventStore.Etl/DataFileImport.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LumenWorks.Framework.IO.Csv;
+
+using Rhino.Etl.Core;
+using Rhino.Etl.Core.Operations;
+
+namespace EventStore.Etl
+{
+    public class DataFileImport : AbstractOperation
+    {
+        private readonly string _filename;
+
+        public DataFileImport(string filename)
+        {
+            _filename = filename;
+        }
+        public override IEnumerable<Row> Execute(IEnumerable<Row> rows)
+        {
+            var reader = new CsvReader(File.OpenText(_filename), true);
+            
+            while (reader.ReadNextRecord())
+            {
+                var row = new Row();
+                row["Id"] = reader["Provider ID"];
+                yield return row;
+            }
+        }
+    }
+}
