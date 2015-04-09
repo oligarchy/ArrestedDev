fd9ad9b23ad667f17c9a7a72df78a450427d455f
jbachmann <jbachmann@nhsinc.com>
2015-04-09 12:39:30 -0400

    Added etl

---
+++ EventStore.Etl/HospitalEtlProcess.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Rhino.Etl.Core;
+
+namespace EventStore.Etl
+{
+    public class HospitalEtlProcess : EtlProcess
+    {
+        protected override void Initialize()
+        {
+            Register(new DataFileImport(@"../../../Data/Hospitals_2014.csv"));
+            Register(new ConsoleWrite());
+        }
+    }
+}
---
+++ EventStore.Etl/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EventStore.Etl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var process = new HospitalEtlProcess();
+            var sw = new Stopwatch();
+            sw.Start();
+            ThreadPool.QueueUserWorkItem(
+                delegate
+                {
+                    while (true)
+                    {
+                        Thread.Sleep(1000);
+                        Console.WriteLine(sw.ElapsedMilliseconds);
+                    }
+                }
+            );
+            process.Execute();
+            sw.Stop();
+            Console.Read();
+        }
+    }
+}
