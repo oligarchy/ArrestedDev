807133bdaca4d3ced8b90bbf10414b949b2a18d0
Justin <justin.joyce@verizon.net>
2015-04-10 10:03:36 -0400

    adding new files and removing unused ones

---
+++ EventStore.Web/Controllers/DetailsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EventStore.Web.Controllers
+{
+    public class DetailsController : Controller
+    {
+        public ActionResult ViewCollections()
+        {
+            return View();
+        }
+
+        public ActionResult ViewEntities()
+        {
+            return View();
+        }
+
+        public ActionResult ViewEntityDetails()
+        {
+            return View();
+        }
+	}
+}
