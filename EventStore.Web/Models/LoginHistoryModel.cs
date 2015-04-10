73b6f7c22e374fb8f28df9f7c871cb9a73e538d2
Justin <justin.joyce@verizon.net>
2015-04-10 16:05:43 -0400

    adding login page

---
+++ EventStore.Web/Models/LoginHistoryModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EventStore.Common;
+
+namespace EventStore.Web.Models
+{
+    public class LoginHistoryModel
+    {
+        public List<Login> Logins { get; set; }
+    }
+}
