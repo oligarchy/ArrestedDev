2bdb8898b60e9e757ec8f3d913e99f2828b9d39e
Justin <justin.joyce@verizon.net>
2015-04-10 11:53:32 -0400

    controller methods and views to drill down through objects

--- EventStore.Web/Controllers/DetailsController.cs
+++ EventStore.Web/Controllers/DetailsController.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using EventStore.Common;
+using EventStore.Web.Models;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
 
 namespace EventStore.Web.Controllers
 {
@@ -10,17 +15,51 @@ namespace EventStore.Web.Controllers
     {
         public ActionResult ViewCollections()
         {
-            return View();
+            ViewCollectionsModel model = new ViewCollectionsModel();
+
+            var mongo = GetMongoDb();
+            var results = mongo.ListCollectionsAsync().Result.ToListAsync().Result;
+
+            foreach (var result in results)
+            {
+                string name = result["name"].ToString();
+
+                if (name != "system.indexes")
+                {
+                    model.Collections.Add(name);
+                }
+            }
+
+            return View(model);
         }
 
-        public ActionResult ViewEntities()
+        public ActionResult ViewEntities(string CollectionName)
+        {
+            ViewEntitiesModel model = new ViewEntitiesModel();
+
+            var mongo = GetMongoDb();
+            IMongoCollection<Hospital> collection = mongo.GetCollection<Hospital>(CollectionName);
+            var results = collection.FindAsync(x => x.Id != null);
+            results.Wait();
+
+            var docs = results.Result.ToListAsync();
+            docs.Wait();
+
+            model.Hospitals = docs.Result;
+
+            return View(model);
+        }
+
+        public ActionResult ViewEntityDetails(string EntityId)
         {
             return View();
         }
 
-        public ActionResult ViewEntityDetails()
+        private IMongoDatabase GetMongoDb()
         {
-            return View();
+            var client = new MongoClient();
+            var db = client.GetDatabase("EventStore");
+            return db;
         }
 	}
 }
